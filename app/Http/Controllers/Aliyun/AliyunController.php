ebb720cd4ea0a8ce61c4da3b3d155497f9bf1c6b
liyijiehaha <2632379213@qq.com>
2019-07-16 09:11:29 +0000

    1

--- app/Http/Controllers/Aliyun/AliyunController.php
+++ app/Http/Controllers/Aliyun/AliyunController.php
@@ -8,6 +8,8 @@ use App\Http\Controllers\Controller;
 class AliyunController extends Controller
 {
     public function CallBack(){
-        echo __METHOD__;
+       $json=file_get_contents("php://input");
+       $logs_str=date('Y-m-d H:i:s').">>>>>".$json."\n";
+       file_put_contents("logs/oss.log",$logs_str,FILE_APPEND);
     }
 }
